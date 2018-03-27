6d2b1f4c46776b408f16b37361b442ffb0cba191
junsikwon <37567362+junsikwon@users.noreply.github.com>
2018-03-27 14:40:26 +0900

    수정

--- hello.js
+++ hello.js
@@ -1,4 +1,4 @@
-var http = require('http');
+/*var http = require('http');
 
 function requestCallbackFunction (req, res) {
   // if(req.url === '/favicon.ico') {
@@ -15,3 +15,32 @@ var server = http.createServer(
 );
 
 server.listen(8080);
+*/
+var http = require('http');
+2 /*
+3 function requestCallbackFunction (req, res) {
+4   // if(req.url === '/favicon.ico') {
+5   //   console.log('Favicon was requested');
+6   // } else {
+7     console.log('hello');
+8   // }
+9   res.writeHead(200, {'Content-Type': 'text/plain'});
+10   res.end('Hello World!');
+11 }
+12 */
+
+
+ var server = http.createServer(
+   function (req, res) {
+    if (req.url === '/favicon.ico') {
+      console.log('Favicon was requested');
+     } else {
+       console.log('hello');
+     }
+     res.writeHead(200, {'Content-Type': 'text/plain'}); 
+     res.end('Hello World!');
+   }
+ );
+
+
+ server.listen(8080);
